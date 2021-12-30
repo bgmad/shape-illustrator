818898491c1b2955ff7f718575f89e5f24c89a10
Ian Luchini <iancarloluchini@gmail.com>
2021-12-29 23:11:57 -0500

    Sum styling w/ media queries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 import Checkbox from './components/Checkbox';
 import RefreshIcon from './components/RefreshIcon';
 import { handleCheckboxChange, handleButton } from './utils/handlers';
+
+const Checklist = styled.form`
+  display: flex;
+  flex-flow: column wrap;
+  min-width: auto;
+  width: 20%;
+  padding-top: .7rem;
+  padding-bottom: .7rem;
+  background: rgb(0, 0, 100, 0.2);
+  label {
+    padding: 0.7rem 0 0.7rem 1rem;
+    font-weight: bold;
+  }
+  @media only screen and (max-width: 800px) {
+    margin-bottom: 2rem;
+    width: 50%;
+    text-align: center;
+  }
+`;
+
+const MainContainer = styled.div`
+  width: 70%;
+  text-align: center;
+  button {
+    margin-bottom: 1.5rem;
+  }
+  h2 {
+    font-size: 3rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+  }
+`;
 
 export default class App extends React.Component {
   constructor() {
@@ -24,7 +57,7 @@ export default class App extends React.Component {
   }
   render() {
     return <div className="App">
-      <form>
+      <Checklist>
         {this.state.shapeSelections.map(s => 
           <Checkbox 
             key={s.name} 
@@ -33,12 +66,14 @@ export default class App extends React.Component {
             handleCheckboxChange={this.handleCheckboxChange}/>
           )
         }
-      </form>
-      <button onClick={this.handleButton}>
-        <RefreshIcon />
-      </button>
-      {this.state.currentShape && <h3>{this.state.currentShape}</h3>}
-      {this.state.prompt && <p>{this.state.prompt}</p>}
+      </Checklist>
+      <MainContainer>
+        <button onClick={this.handleButton}>
+          <RefreshIcon />
+        </button>
+        {this.state.currentShape && <h2>{this.state.currentShape}</h2>}
+        {this.state.prompt && <p>{this.state.prompt}</p>}
+      </MainContainer>
     </div>
   }   
 }
